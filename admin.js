2a993a5444bb9cd4443bce5c6268f7035cc8efe0
ChancellorCeti <74802589+ChancellorCeti@users.noreply.github.com>
2022-04-20 21:54:47 -0500

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-/*var config = {
+var config = {
     apiKey: "AIzaSyCuxrJZUhn9HYFPY8zXT6fW5frDq_JGfTo",
       authDomain: "minikyt-3aa38.firebaseapp.com",
       projectId: "minikyt-3aa38",
@@ -36,20 +36,6 @@ db.collection('signups').get().then(snapshot => {
     snapshot.docs.forEach(doc => {
         renderCafe(doc);
     });
-});*/
-console.log(Math.sqrt(9))
-var a=prompt('a value')
-var b=prompt('b value')
-var c=prompt('c value')
-var sol1a=(-1*b)+Math.sqrt(Math.pow(b,2)-4*a*c)
-var sol1=sol1a/(2*a)
+});
 
-
-var sol2a=(-1*b)-Math.sqrt(Math.pow(b,2)-4*a*c)
-var sol2=sol2a/(2*1)
-if(sol1==NaN&&sol2==NaN){
-    alert('no solution-- gimme a real quadratic stoopid');
-}else{
-alert('solutions are '+sol1+" and "+sol2)
-}
 
